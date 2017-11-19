54c994a9b1a7ece718b65f2666242a4b71f26508
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2017-11-19 17:34:46 +0900

    [Remove] - Excluded the build folder from git management

--- build/app/routes/middlewares/errorMiddleware.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  module.exports = function(app) {
-    return app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      return res.send(err);
-    });
-  };
-
-}).call(this);
